f82442ec1dd455e602997137bba40d784b10f212
Destaq <trilogicworlds@gmail.com>
2020-09-23 11:27:50 +0200

    add common error pages

--- init.py
+++ init.py
@@ -75,6 +75,14 @@ def form_application() -> Flask:
     def page_not_found(e):
         return render_template("404.html"), 404
 
+    @app.errorhandler(403)
+    def access_forbidden(e):
+        return render_template("403.html"), 403
+
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template("500.html"), 500
+
     register_views(app)
 
     return app
